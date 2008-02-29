fd225ea9466f94f970cf219b1de5f2ef645a425e
James Oakley <jfunk@funktronics.ca>
2008-02-29 16:08:25 +0000

    Initial import of yabsc, Yet Another Build Service Client

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+long_description = "An openSUSE Build Service client that focuses on package build results"
+
+setup(name             = "yabsc",
+      version	       = "0.2",
+      description      = "Yet Another Build Service Client",
+      long_description = long_description,
+      author           = "James Oakley",
+      author_email     = "jfunk@opensuse.org",
+      url              = "http://www.funktronics.ca/yabsc",
+      license	       = "GPL",
+      scripts          = ['yabsc'],
+     )
